02fd14796f888acc6a1ac33cc7a32f6f5303cfb4
ISHA <65284360+IshaGoel22@users.noreply.github.com>
2024-01-14 11:13:16 +0530

    Add files via upload

---
+++ places_list/lib/model/places.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+class PlaceLocation {
+  final double latitude;
+  final double longitude;
+  final String address;
+
+  const PlaceLocation({
+    required this.latitude,
+    required this.longitude,
+    required this.address,
+  });
+}
+
+class Place {
+  final String id;
+  final String title;
+  final File image;
+  final PlaceLocation location;
+
+  Place(
+      {required this.location,
+      required this.id,
+      required this.image,
+      required this.title});
+}
